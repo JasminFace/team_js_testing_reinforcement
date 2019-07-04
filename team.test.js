367ac51c7441376e4e30e9fcd6fe4536642e61a9
Jasmin Saromo <jasminsaromo@gmail.com>
2019-07-04 14:49:33 -0400

    completed assignment

--- team.test.js
+++ team.test.js
@@ -1,5 +1,6 @@
 const teamObj = require('./team.js');
 
-// test('', () => {
-//
-// });
+test('test that new team objects is created', () => {
+  const result = teamObj('Unicron', 'Master')
+  expect(result).toEqual({'teamName': 'Unicron', 'level': 'Master', 'points': 0})
+});
